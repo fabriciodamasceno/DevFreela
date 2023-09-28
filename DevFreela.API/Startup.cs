7c05c00613e8f93ba0ba860038d64e2b1172680b
Fabrício M. Damasceno <fabricio.damasceno@compasso.com.br>
2023-09-28 13:22:10 -0300

    Implementando OpeningTime na aplicação

--- DevFreela.API/Startup.cs
+++ DevFreela.API/Startup.cs
@@ -1,3 +1,4 @@
+using DevFreela.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,6 +20,7 @@ namespace DevFreela.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<OpeningTimeOption>(Configuration.GetSection("OpeningTime"));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
